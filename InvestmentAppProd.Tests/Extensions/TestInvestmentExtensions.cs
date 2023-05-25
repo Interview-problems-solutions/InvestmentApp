121d9d997675ee077953a2373d801a4aa935c5c9
Dileep <zX1104221@wowcorp.com.au>
2023-05-26 11:32:17 +1200

    Add unit tests for Interest calculation

---
+++ InvestmentAppProd.Tests/Extensions/TestInvestmentExtensions.cs
@@ -0,0 +1,41 @@
+﻿namespace InvestmentAppProd.Tests.Extensions;
+
+internal class TestInvestmentExtensions
+{
+    [TestCase(-2, 3.875, 10000, ExpectedResult = 10804.47)]
+    [TestCase(-2.5, 3.875, 10000, ExpectedResult = 11015.51)]
+    public double CalculateInterest_For_CompoundInterest_ShouldReturn_CorrectValue(
+        double numberOfYears,
+        double interestRate,
+        double principal)
+    {
+        var investment = new Investment(
+            "Compound Investment",
+            DateTime.Now.AddMonths(ToMonths(numberOfYears)),
+            "Compound",
+            interestRate,
+            principal);
+
+        return investment.CalculateInterest();
+    }
+
+    [TestCase(-2, 3.875, 10000, ExpectedResult = 10775.00)]
+    [TestCase(-2.5, 3.875, 10000, ExpectedResult = 10968.75)]
+    public double CalculateInterest_For_Simple_Interest_Should_Return_CorrectValue(
+        double numberOfYears,
+        double interestRate,
+        double principal)
+    {
+        var investment = new Investment(
+            "Simple Investment",
+            DateTime.Now.AddMonths(ToMonths(numberOfYears)),
+            "Simple",
+            interestRate,
+            principal);
+
+        return investment.CalculateInterest();
+    }
+
+    private static int ToMonths(double numberOfYears) 
+        => (int)Math.Round(numberOfYears * 12, MidpointRounding.AwayFromZero);
+}
--- InvestmentAppProd.Tests/GlobalUsings.cs
+++ InvestmentAppProd.Tests/GlobalUsings.cs
@@ -21,3 +21,4 @@ global using System.Collections.Generic;
 global using System.Linq;
 global using System.Threading;
 global using System.Threading.Tasks;
+global using InvestmentAppProd.Extensions;
